b1e23628ab9a68bcd27d4dc95da632fcb38c87b7
joeb42 <70596457+joeb42@users.noreply.github.com>
2023-01-07 17:35:08 +0000

    use pathlib instead of strings

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -8,12 +8,12 @@ MERGED_PATH = DATA_PATH / "london_energy_and_weather.csv"
 
 
 def merge_datasets():
-    energy_df = pd.read_csv("data/london_energy.csv")
-    weather_df = pd.read_csv("data/london_weather.csv")
+    energy_df = pd.read_csv(DATA_PATH / "london_energy.csv")
+    weather_df = pd.read_csv(DATA_PATH / "london_weather.csv")
     weather_df = weather_df.rename(columns={"date": "dateval"})
     weather_df["date"] = pd.to_datetime(weather_df["dateval"], format="%Y%m%d")
     energy_df["date"] = energy_df["Date"].apply(datetime.strptime, args=("%Y-%m-%d",))
-    energy_df.drop("Date", axis=1)
+    energy_df = energy_df.drop("Date", axis=1)
     merged_df = pd.merge(weather_df, energy_df, on="date")
     return merged_df
 
